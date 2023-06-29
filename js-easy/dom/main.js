337b1a0b081065fb85d602ca3a322a9a7221fe89
kkcom284 <kkcom9100@gmail.com>
2023-06-29 17:51:00 +0900

    window, document

---
+++ js-easy/dom/main.js
@@ -0,0 +1,9 @@
+// アラート表示
+window.alert('これはwindow.alert()で表示したアラートです')
+
+// window. に限り、省略することができる。
+alert('これはalert()で表示したアラートです')
+
+// documentのプロパティにアクセスする例
+console.log(document.doctype) //=> <!doctype html>
+console.log(document.title) //=> DOM API
