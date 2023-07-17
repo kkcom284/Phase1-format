278d1315218fd5c61683fcc65baabca2249a72ab
kkcom284 <kkcom9100@gmail.com>
2023-07-18 08:16:51 +0900

    追加チャレンジ

--- js-easy/saitosan/main.js
+++ js-easy/saitosan/main.js
@@ -1,6 +1,21 @@
-const setBtn = function() {
-const hoge = Math.random()*10;
-    
-   hoge <5?alert('斎藤さんだぞ'):alert('ペッペペー');
+const word1='斎藤さんだぞぉぉぉぉぉ';
+const word2='ペッペペー';
 
+const setBtn2=function(){
+    const foo =Math.random()*10;
+    if(foo<5){
+        const result= window.confirm(word1);
+        if(result){
+            alert(word1);
+        }else{
+            alert(word2);
+        }
+        }else {
+            const result =window.confirm(word2);
+            if(result){
+                alert(word2);
+            }else{
+                alert(word2);
+        }  
+    }
 };
