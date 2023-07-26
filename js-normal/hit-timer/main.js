0edb9402a1d380de5e856e65160a3b424303da83
kkcom284 <kkcom9100@gmail.com>
2023-07-26 21:28:04 +0900

    hitTimer01

--- js-normal/hit-timer/main.js
+++ js-normal/hit-timer/main.js
@@ -1,2 +1,31 @@
-const confirmBtn = document.getElementById('confirmTime');
-const startBtn = document.getElementById('startTimer');
+let timer; /*繰り返し構文＝let timer を繰り返す */
+let sec = 0; /*letは0である*/
+
+const confirmTime = document.getElementById('confirmTime'); 
+const startTimer = document.getElementById('startTimer');
+
+confirmTime.addEventListener('click', function() {
+  if (sec === 20) {
+    alert('大正解^-^');
+  } else if (sec < 20) {
+    alert(`まだ${sec}秒、、、再スタートだ`);
+  } else {
+    alert(`もう${sec}秒だ！再挑戦せよ`);
+  }
+  clearInterval(timer);
+  sec = 0;
+});
+
+startTimer.addEventListener('click', function() {
+  clearInterval(timer);
+  timer = setInterval('countUp()', 1000);
+});
+
+const countUp = function() {
+  sec += 1;
+  if (sec === 40) {
+    clearInterval(timer);
+    alert('終了/また挑戦しろ');
+    sec = 0;
+  }
+};
