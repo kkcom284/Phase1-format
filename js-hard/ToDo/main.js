8faf325ce5df7dabebed113041ae562e02aeb548
kkcom284 <kkcom9100@gmail.com>
2023-08-09 23:09:38 +0900

    002

--- js-hard/ToDo/main.js
+++ js-hard/ToDo/main.js
@@ -1,30 +1,32 @@
-const addli=document.getElementsByClassName('addli');
-const addbtn =document.getElementsByClassName('add-btn');
-const lists = document.getElementsByClassName('lists');
+const addli = document.getElementsByClassName("addli")[0];
+const addbtn = document.getElementsByClassName("add-btn")[0];
+const lists = document.getElementsByClassName("lists")[0];
+const todo = document.getElementById("todo");
 
 const addTasks = (task) => {
-  const listItem = document.createElement('li');
-  const showItem = taskList.appendChild(listItem);
-  showItem.innerHTML = task;
+  const listItem = document.createElement("li");
+  listItem.innerHTML = task;
+  todo.appendChild(listItem);
 
-  const deleteButton = document.createElement('button');
-  deleteButton.innerHTML = 'Delete';
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "Delete";
   listItem.appendChild(deleteButton);
 
-  deleteButton.addEventListener('click', evt => {
+  deleteButton.addEventListener("click", (evt) => {
     evt.preventDefault();
     deleteTasks(deleteButton);
   });
 };
 
 const deleteTasks = (deleteButton) => {
-  const chosenTask = deleteButton.closest('li');
-  taskList.removeChild(chosenTask);
+  const chosenTask = deleteButton.closest("li");
+  todo.removeChild(chosenTask);
 };
 
-taskSubmit.addEventListener('click', evt => {
+addbtn.addEventListener("click", (evt) => {
   evt.preventDefault();
+  const taskValue = document.getElementById("add-area");
   const task = taskValue.value;
   addTasks(task);
-  taskValue.value = '';
+  taskValue.value = "";
 });
