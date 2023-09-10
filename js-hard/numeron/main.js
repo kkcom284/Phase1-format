731bc306787c03fb76397dfe3bc9148ae7627e1a
kkcom284 <kkcom9100@gmail.com>
2023-09-10 22:05:44 +0900

    001

--- js-hard/numeron/main.js
+++ js-hard/numeron/main.js
@@ -1,2 +1,24 @@
 const answerNum = document.getElementById("answerNum");
 const numCheck = document.getElementById("numCheck");
+const remainTurn = document.getElementById("remainTurn");
+
+const raondomNum = Math.floor(Math.random() * 9 + 1);
+
+const firstNum = array[0][rondomNum];
+
+// let number = [
+//   [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+//   [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+//   [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+// ];
+
+let;
+// console.log(number[2][1]);
+
+// let userData = [
+//   ["Yamada", 28, "Tokyo"],
+//   ["Suzuki", 35, "Fukuoka"],
+//   ["Honda", 24, "Sendai"],
+// ];
+// console.log(userData[1][0]);
+// console.log(userData[2][2]);
