af4ff0e052e8d6113b4e233660669b51e6f40c19
kkcom284 <kkcom9100@gmail.com>
2023-10-11 23:51:04 +0900

    ビンゴシート

--- js-hard/bingosheet/main.js
+++ js-hard/bingosheet/main.js
@@ -71,46 +71,131 @@ const td30 = document.createElement("td");
 // th.textContent = "b";
 
 td1.textContent = "B";
-td6.textContent = Math.floor(Math.random() * 15 + 1);
-td11.textContent = Math.floor(Math.random() * 15 + 1);
-td16.textContent = Math.floor(Math.random() * 15 + 1);
-td21.textContent = Math.floor(Math.random() * 15 + 1);
-td26.textContent = Math.floor(Math.random() * 15 + 1);
-if (
-  td6 !== td11 &&
-  td11 !== td16 &&
-  td16 !== td21 &&
-  td21 !== td26 &&
-  td26 !== td6
-)
-  td2.textContent = "I";
-td7.textContent = String(Math.floor(Math.random() * (30 - 15) + 15 + 1));
-td12.textContent = String(Math.floor(Math.random() * (30 - 15) + 15 + 1));
-td17.textContent = String(Math.floor(Math.random() * (30 - 15) + 15 + 1));
-td22.textContent = String(Math.floor(Math.random() * (30 - 15) + 15 + 1));
-td27.textContent = String(Math.floor(Math.random() * (30 - 15) + 15 + 1));
+
+// 1~15までの数字をランダムに生成５つ
+let b = [];
+// while (true) {
+for (let i = 0; i < 5; i++) {
+  while (true) {
+    sheet1 = Math.floor(Math.random() * (1 + 15 - 1)) + 1;
+
+    b.includes(sheet1);
+    if (!b.includes(sheet1)) {
+      b.push(sheet1);
+      break;
+    }
+  }
+}
+
+td6.textContent = b[0];
+td11.textContent = b[1];
+td16.textContent = b[2];
+td21.textContent = b[3];
+td26.textContent = b[4];
+console.log(b);
+console.log(b.includes(sheet1));
+console.log(typeof b);
+
+let i2 = [];
+for (let q = 0; q < 5; q++) {
+  while (true) {
+    sheet2 = Math.floor(Math.random() * (1 + 30 - 16)) + 16;
+
+    i2.includes(sheet2);
+    if (!i2.includes(sheet2)) {
+      i2.push(sheet2);
+      break;
+    }
+  }
+}
+td7.textContent = i2[0];
+td12.textContent = i2[1];
+td17.textContent = i2[2];
+td22.textContent = i2[3];
+td27.textContent = i2[4];
+console.log(i2);
+let n = [];
+for (let w = 0; w < 4; w++) {
+  while (true) {
+    sheet3 = Math.floor(Math.random() * (1 + 45 - 31)) + 31;
+
+    n.includes(sheet3);
+    if (!n.includes(sheet3)) {
+      n.push(sheet3);
+      break;
+    }
+  }
+}
+td8.textContent = n[0];
+td13.textContent = n[1];
+td23.textContent = n[2];
+td28.textContent = n[3];
+
+let g = [];
+for (let e = 0; e < 5; e++) {
+  while (true) {
+    sheet4 = Math.floor(Math.random() * (1 + 60 - 46)) + 46;
+
+    g.includes(sheet4);
+    if (!g.includes(sheet4)) {
+      g.push(sheet4);
+      break;
+    }
+  }
+}
+td9.textContent = g[0];
+td14.textContent = g[1];
+td19.textContent = g[2];
+td24.textContent = g[3];
+td29.textContent = g[4];
+
+let o = [];
+for (let r = 0; r < 5; r++) {
+  while (true) {
+    sheet5 = Math.floor(Math.random() * (1 + 75 - 61)) + 61;
+
+    o.includes(sheet5);
+    if (!o.includes(sheet5)) {
+      o.push(sheet5);
+      break;
+    }
+  }
+}
+td10.textContent = o[0];
+td15.textContent = o[1];
+td20.textContent = o[2];
+td25.textContent = o[3];
+td30.textContent = o[4];
+
+// 配列を作っていれる
+td2.textContent = "I";
+// td7.textContent = Math.floor(Math.random() * (30 - 15) + 15 + 1);
+// td12.textContent = Math.floor(Math.random() * (30 - 15) + 15 + 1);
+// td17.textContent = Math.floor(Math.random() * (30 - 15) + 15 + 1);
+// td22.textContent = Math.floor(Math.random() * (30 - 15) + 15 + 1);
+// td27.textContent = Math.floor(Math.random() * (30 - 15) + 15 + 1);
 
 td3.textContent = "N";
-td8.textContent = String(Math.floor(Math.random() * (45 - 30) + 30 + 1));
-td13.textContent = String(Math.floor(Math.random() * (45 - 30) + 30 + 1));
-td18.textContent = String(Math.floor(Math.random() * (45 - 30) + 30 + 1));
-td23.textContent = String(Math.floor(Math.random() * (45 - 30) + 30 + 1));
-td28.textContent = String(Math.floor(Math.random() * (45 - 30) + 30 + 1));
+// td8.textContent = Math.floor(Math.random() * (45 - 30) + 30 + 1);
+// td13.textContent = Math.floor(Math.random() * (45 - 30) + 30 + 1);
+td18.textContent = "free";
+// td23.textContent = Math.floor(Math.random() * (45 - 30) + 30 + 1);
+// td28.textContent = Math.floor(Math.random() * (45 - 30) + 30 + 1);
 
 td4.textContent = "G";
-td9.textContent = String(Math.floor(Math.random() * (60 - 45) + 45 + 1));
-td14.textContent = String(Math.floor(Math.random() * (60 - 45) + 45 + 1));
-td19.textContent = String(Math.floor(Math.random() * (60 - 45) + 45 + 1));
-td24.textContent = String(Math.floor(Math.random() * (60 - 45) + 45 + 1));
-td29.textContent = String(Math.floor(Math.random() * (60 - 45) + 45 + 1));
+// td9.textContent = Math.floor(Math.random() * (60 - 45) + 45 + 1);
+// td14.textContent = Math.floor(Math.random() * (60 - 45) + 45 + 1);
+// td19.textContent = Math.floor(Math.random() * (60 - 45) + 45 + 1);
+// td24.textContent = Math.floor(Math.random() * (60 - 45) + 45 + 1);
+// td29.textContent = Math.floor(Math.random() * (60 - 45) + 45 + 1);
 
 td5.textContent = "O";
-td10.textContent = String(Math.floor(Math.random() * (75 - 60) + 60 + 1));
-td15.textContent = String(Math.floor(Math.random() * (75 - 60) + 60 + 1));
-td20.textContent = String(Math.floor(Math.random() * (75 - 60) + 60 + 1));
-td25.textContent = String(Math.floor(Math.random() * (75 - 60) + 60 + 1));
-td30.textContent = String(Math.floor(Math.random() * (75 - 60) + 60 + 1));
-//  trにtdを入れる
+// td10.textContent = Math.floor(Math.random() * (75 - 60) + 60 + 1);
+// td15.textContent = Math.floor(Math.random() * (75 - 60) + 60 + 1);
+// td20.textContent = Math.floor(Math.random() * (75 - 60) + 60 + 1);
+// td25.textContent = Math.floor(Math.random() * (75 - 60) + 60 + 1);
+// td30.textContent = Math.floor(Math.random() * (75 - 60) + 60 + 1);
+// //  trにtdを入れる
 mtable.appendChild(tbody);
 
 tbody.appendChild(tr1);
@@ -122,40 +207,41 @@ tr1.appendChild(td4);
 tr1.appendChild(td5);
 
 tbody.appendChild(tr2);
-tr2.appendChild(td6);
-tr2.appendChild(td7);
-tr2.appendChild(td8);
-tr2.appendChild(td9);
-tr2.appendChild(td10);
-
 tbody.appendChild(tr3);
-tr3.appendChild(td11);
-tr3.appendChild(td12);
-tr3.appendChild(td13);
-tr3.appendChild(td14);
-tr3.appendChild(td15);
-
 tbody.appendChild(tr4);
-tr4.appendChild(td16);
-tr4.appendChild(td17);
-tr4.appendChild(td18);
-tr4.appendChild(td19);
-tr4.appendChild(td20);
-
 tbody.appendChild(tr5);
-tr5.appendChild(td21);
-tr5.appendChild(td22);
-tr5.appendChild(td23);
-tr5.appendChild(td24);
-tr5.appendChild(td25);
-
 tbody.appendChild(tr6);
+
+tr2.appendChild(td6);
+tr3.appendChild(td11);
+tr4.appendChild(td16);
+tr5.appendChild(td21);
 tr6.appendChild(td26);
+
+tr2.appendChild(td7);
+tr3.appendChild(td12);
+tr4.appendChild(td17);
+tr5.appendChild(td22);
 tr6.appendChild(td27);
+
+tr2.appendChild(td8);
+tr3.appendChild(td13);
+tr4.appendChild(td18);
+tr5.appendChild(td23);
 tr6.appendChild(td28);
+
+tr2.appendChild(td9);
+tr3.appendChild(td14);
+tr4.appendChild(td19);
+tr5.appendChild(td24);
 tr6.appendChild(td29);
+
+tr2.appendChild(td10);
+tr3.appendChild(td15);
+tr4.appendChild(td20);
+tr5.appendChild(td25);
 tr6.appendChild(td30);
 
-console.log(view);
+// console.log(view);
 
 // ｔｒにＤＯＭ操作でテーブルタグに追加する
