62fcca4195d0e95dd1876e13ea6cc3098ce71e97
kkcom284 <kkcom9100@gmail.com>
2023-10-24 21:02:56 +0900

    神経衰弱

--- js-hard/sinkeisuizyaku/main.js
+++ js-hard/sinkeisuizyaku/main.js
@@ -3,32 +3,287 @@
 // 数字が一致したら画面から削除/しなかったら裏戻る
 // 削除/裏返しの実行は 2枚目をクリックしてから0.5秒後
 
-// ＜フロー＞
-// panelをindex.htmlより取得
-// // スタイルシート参照
-// ｈｔｍｌのpanel内で作成
-// ８つのカードを生成.card
-// 条件が合わければ.backにする
-//条件が合えば.finishにする
+// カードの並びはページ読み込み時に完了する
+// 一度生成した並びはゲーム終了するまで初期化しない
 
+// ＜フロー＞処理順に書く
+// 最初は大雑把に
+//大雑把に書いた文の間に具体的な物を書く
+
+//①　div=id panelをindex.htmlより取得
+// panel内でゲーム作成
+// 枠となるdivを８つ生成
+
+// ②
+// jsにてcardを生成
+// ８つのカードを生成
+// jsで生成したdiv内で生成と表示
 // 8つのカードの中から2つ同じ物をランダムに１～４の数字を生成する
-//ダステンフェルドのアルゴリズムか分割代入を使う？
-// 1枚目のカードをクリックしたら数字を表示
-// ２枚目のカードをクリックしたら数字を表示
-//
+//ダステンフェルドのアルゴリズムか分割代入を使う？＝分割代入を使用
+// 1枚目のカードをクリックしたら数字を表示＝cardbackからスタート
+// ２枚目のカードをクリックしたら数字を表示＝cardbackからスタート
+
 // イベントリスターを使う＝カードをクリックしたら動作する
-// 一度生成したものは1ゲーム終了するまで初期化しない
 
+// ③
+// jsにてclass属性backを生成
+// backをdivに追加
+// jsで生成したdiv内で生成と表示
+// 2つカードが同じか否かをチェックする
+// ２つのカードが同じ数字でなければ.backにする
+
+// ④
+// jsにてfinisを生成する
+// jsで生成したdiv内で生成と表示
+//2つのカードが揃えば.fnishにする
+// イベントリスターを使う＝カードをクリックしたら動作する
 // ２つの数字が合っているかor合っていないかチェック
-// 合っていれば画面から削除合っていなければ前段階に戻る＝同じ数字を選択
 // 2つのカードをクリックして500ミリ秒で動作する＝ setinterbel
+// 合っていれば画面から削除合っていなければ前段階に戻る＝同じ数字を選択
 
-//全てカードが削除したらアラート”終了です”
+//⑤　全てのカードが削除したらアラート”終了です”
 
+// ① 12行目
 const panel = document.getElementById("panel");
-let backCard = document.querySelector(".back");
-let finishCard = document.querySelector(".finish");
 
-for (i = 0; i < 8; i++) {
-  let div = document.createElement("div");
+let fragment = document.createDocumentFragment();
+
+// 複数のノード
+let cardElments = document.getElementsByClassName("card");
+let cardBack = document.getElementsByClassName("back");
+let cardFinish = document.getElementsByClassName("finish");
+
+let orderNum = [1, 1, 2, 2, 3, 3, 4, 4];
+let randomNum = [];
+let rand;
+for (let i = 0, len = orderNum.length; i < 8; i++, len--) {
+  rand = Math.floor(Math.random() * len);
+  randomNum.push(orderNum.splice(rand, 1)[0]);
+  console.log("orderNum:" + orderNum);
+  console.log("randomNum:" + randomNum);
 }
+
+// 目標　8つの１～４の数字を持ったcardというclassを持つdivを作る
+//できたこと
+// divの作成
+// １～４の数字を作る
+// divにcardというclassを付ける
+// 作ったdivタグを写す
+//１～４の数字をdivのinnerHTMLに入れる
+
+// できていないこと
+// １～４の数字を２つずつ作る
+
+// やりたいこと
+// [1,1,2,2,3,3,4,4];を作る
+// ランダムに取り出す　方法は調べる
+// divに入れる
+
+// randomNumが８になるまで
+div1 = document.createElement("div");
+div2 = document.createElement("div");
+div3 = document.createElement("div");
+div4 = document.createElement("div");
+div5 = document.createElement("div");
+div6 = document.createElement("div");
+div7 = document.createElement("div");
+div8 = document.createElement("div");
+
+div1.classList.add("card", "back");
+div2.classList.add("card", "back");
+div3.classList.add("card", "back");
+div4.classList.add("card", "back");
+div5.classList.add("card", "back");
+div6.classList.add("card", "back");
+div7.classList.add("card", "back");
+div8.classList.add("card", "back");
+
+// calss作成
+fragment.append(div1);
+fragment.append(div2);
+fragment.append(div3);
+fragment.append(div4);
+fragment.append(div5);
+fragment.append(div6);
+fragment.append(div7);
+fragment.append(div8);
+
+panel.after(div1);
+panel.after(div2);
+panel.after(div3);
+panel.after(div4);
+panel.after(div5);
+panel.after(div6);
+panel.after(div7);
+panel.after(div8);
+
+let firstCard = null;
+let secondCard = null;
+const flip = (eve) => {
+  let div1 = eve.target;
+  if (!div1.classList.contains("back") || secondCard !== null) {
+    return;
+  }
+};
+
+div1.addEventListener("click", () => {
+  div1.classList.remove("back");
+  if (firstCard === null) {
+    firstCard = div1;
+  } else {
+    secondCard = div1;
+    if (firstCard.num === secondCard.num) {
+      firstCard.classList.add("finish");
+      secondCard.classList.add("finish");
+      [firstCard, secondCard] = [null, null];
+    } else {
+      firstCard.classList.add("back");
+      secondCard.classList.add("back");
+      [firstCard, secondCard] = [null, null];
+    }
+  }
+  div1.innerHTML = randomNum[0];
+});
+
+div2.addEventListener("click", () => {
+  div2.classList.remove("back");
+  if (firstCard === null) {
+    firstCard = div2;
+  } else {
+    secondCard = div2;
+    if (firstCard.num === secondCard.num) {
+      firstCard.classList.add("finish");
+      secondCard.classList.add("finish");
+      [firstCard, secondCard] = [null, null];
+    } else {
+      firstCard.classList.add("back");
+      secondCard.classList.add("back");
+      [firstCard, secondCard] = [null, null];
+    }
+  }
+  div2.innerHTML = randomNum[1];
+});
+
+div3.addEventListener("click", () => {
+  div3.classList.remove("back");
+  if (firstCard === null) {
+    firstCard = div3;
+  } else {
+    secondCard = div3;
+    if (firstCard.num === secondCard.num) {
+      firstCard.classList.add("finish");
+      secondCard.classList.add("finish");
+      [firstCard, secondCard] = [null, null];
+    } else {
+      firstCard.classList.add("back");
+      secondCard.classList.add("back");
+      [firstCard, secondCard] = [null, null];
+    }
+  }
+  div3.innerHTML = randomNum[2];
+});
+div4.addEventListener("click", () => {
+  div4.classList.remove("back");
+  if (firstCard === null) {
+    firstCard = div4;
+  } else {
+    secondCard = div4;
+    if (firstCard.num === secondCard.num) {
+      firstCard.classList.add("finish");
+      secondCard.classList.add("finish");
+      [firstCard, secondCard] = [null, null];
+    } else {
+      firstCard.classList.add("back");
+      secondCard.classList.add("back");
+      [firstCard, secondCard] = [null, null];
+    }
+  }
+  div4.innerHTML = randomNum[3];
+});
+div5.addEventListener("click", () => {
+  div5.classList.remove("back");
+  if (firstCard === null) {
+    firstCard = div5;
+  } else {
+    secondCard = div5;
+    if (firstCard.num === secondCard.num) {
+      firstCard.classList.add("finish");
+      secondCard.classList.add("finish");
+      [firstCard, secondCard] = [null, null];
+    } else {
+      firstCard.classList.add("back");
+      secondCard.classList.add("back");
+      [firstCard, secondCard] = [null, null];
+    }
+  }
+  div5.innerHTML = randomNum[4];
+});
+div6.addEventListener("click", () => {
+  div6.classList.remove("back");
+  if (firstCard === null) {
+    firstCard = div6;
+  } else {
+    secondCard = div6;
+    if (firstCard.num === secondCard.num) {
+      firstCard.classList.add("finish");
+      secondCard.classList.add("finish");
+      [firstCard, secondCard] = [null, null];
+    } else {
+      firstCard.classList.add("back");
+      secondCard.classList.add("back");
+      [firstCard, secondCard] = [null, null];
+    }
+  }
+  div6.innerHTML = randomNum[5];
+});
+div7.addEventListener("click", () => {
+  div7.classList.remove("back");
+  if (firstCard === null) {
+    firstCard = div7;
+  } else {
+    secondCard = div7;
+    if (firstCard.num === secondCard.num) {
+      firstCard.classList.add("finish");
+      secondCard.classList.add("finish");
+      [firstCard, secondCard] = [null, null];
+    } else {
+      firstCard.classList.add("back");
+      secondCard.classList.add("back");
+      [firstCard, secondCard] = [null, null];
+    }
+  }
+  div7.innerHTML = randomNum[6];
+});
+div8.addEventListener("click", () => {
+  div8.classList.remove("back");
+  if (firstCard === null) {
+    firstCard = div8;
+  } else {
+    secondCard = div8;
+    if (firstCard.num === secondCard.num) {
+      firstCard.classList.add("finish");
+      secondCard.classList.add("finish");
+      [firstCard, secondCard] = [null, null];
+    } else {
+      firstCard.classList.add("back");
+      secondCard.classList.add("back");
+      [firstCard, secondCard] = [null, null];
+    }
+  }
+  div8.innerHTML = randomNum[7];
+});
+// div2.innerHTML = randomNum[1];
+// div3.innerHTML = randomNum[2];
+// div4.innerHTML = randomNum[3];
+// div5.innerHTML = randomNum[4];
+// div6.innerHTML = randomNum[5];
+// div7.innerHTML = randomNum[6];
+// div8.innerHTML = randomNum[7];
+
+// console.log(orderNum);
+console.log(cardElments);
+// ②１６行目
+
+// ③　２2行目
+
+// ④＋⑤　２９行目＋３７行目
